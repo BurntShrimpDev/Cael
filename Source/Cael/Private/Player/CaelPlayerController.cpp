aca41250f852b92d942e1eb2c1ad692326f08006
Callum Brogan <brogancsp@gmail.com>
2025-04-12 22:56:11 +0200

    Movement Input

--- Source/Cael/Private/Player/CaelPlayerController.cpp
+++ Source/Cael/Private/Player/CaelPlayerController.cpp
@@ -3,3 +3,56 @@
 
 #include "Player/CaelPlayerController.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+ACaelPlayerController::ACaelPlayerController()
+{
+	// For future multiplayer support
+	bReplicates = true;	
+}
+
+void ACaelPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(GameplayContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(GameplayContext, GameplayContextPriority);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void ACaelPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ACaelPlayerController::Move);
+}
+
+void ACaelPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControllerPawn = GetPawn<APawn>())
+	{
+		ControllerPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControllerPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+	
+}
--- Source/Cael/Public/Player/CaelPlayerController.h
+++ Source/Cael/Public/Player/CaelPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "CaelPlayerController.generated.h"
 
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
 /**
  * 
  */
@@ -13,5 +16,37 @@ UCLASS()
 class CAEL_API ACaelPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+
+	ACaelPlayerController();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+
+private:
+
+	// Input Mapping Contexts
 	
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> GameplayContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	int32 GameplayContextPriority = 0;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> MenuContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	int32 MenuContextPriority = 1;
+
+	// Input Actions
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
