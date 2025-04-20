29c48b9ece6a5e9d2665d4fcde6b6c007dd65023
Callum Brogan <brogancsp@gmail.com>
2025-04-20 04:09:15 +0200

    CommonUI Setup & Exploration

--- Source/Cael/Cael.h
+++ Source/Cael/Cael.h
@@ -4,3 +4,12 @@
 
 #include "CoreMinimal.h"
 
+// Material Parameter References
+#define CAEL_MAT_PARAM_NAME_TEXTURE			FName(TEXT("Texture"))
+#define CAEL_MAT_PARAM_NAME_HOVER			FName(TEXT("Hover_Animate"))
+#define CAEL_MAT_PARAM_TEX_ALPHA				FName(TEXT("TextureAlpha"))
+#define CAEL_MAT_PARAM_TEX_HOVER_ALPHA		FName(TEXT("TextureHoverAlpha"))
+#define CAEL_MAT_PARAM_TEX_SCALE				FName(TEXT("TextureScale"))
+#define CAEL_MAT_PARAM_TEX_HOVER_SCALE 		FName(TEXT("TextureHoverScale"))
+#define CAEL_MAT_PARAM_TEX_SHIFT_X			FName(TEXT("Shift_X"))
+#define CAEL_MAT_PARAM_TEX_SHIFT_Y			FName(TEXT("Shift_Y"))
